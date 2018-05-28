b011cf4e13b55f8d06f061696b80c259a09464dc
Vip-X TI <vipxti@gmail.com>
2018-05-28 15:57:59 -0300

    Changed index page

---
+++ resources/views/partials/_head.blade.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Celestial</title>
+
+<!-- Favicon  -->
+<link rel="icon" href="{{ asset('img/core-img/favicon.ico') }}">
+
+@include('partials._styles')
---
+++ resources/views/partials/_styles.blade.php
@@ -0,0 +1,6 @@
+<!-- Core Style CSS -->
+<link rel="stylesheet" href="{{ asset('css/core-style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+<!-- Responsive CSS -->
+<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
