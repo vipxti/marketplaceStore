6c9fb9848043c2552f899e130645c555eba75167
Myck Carvalho <myckk.10@gmail.com>
2018-06-22 17:04:16 -0300

    Update

--- resources/views/pages/app/checkout.blade.php
+++ resources/views/pages/app/checkout.blade.php
@@ -88,134 +88,6 @@
                                         </div>
                                     </div>
                                 </div>
-                                <br><br><br>
-                                <br><br><br>
-                                <br><br><br>
-
-                                <!-- Botão continuar  -->
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-md-12" style="text-align: right">
-                                           <a class="btn" id="next" style="color: #fff; margin-top: 6px">Continuar &nbsp;<i class="fa fa-angle-double-right"></i> </a>
-                                           <input type="submit" class="btn">
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <!-- Detalhes do produto -->
-                            <div class="col-md-5 text-center">
-                                <div class="order-details-confirmation">
-
-                                    <!-- Imagem do Produto -->
-                                    <span>
-                                            <img class="badge__product-icon picture-image" src="https://http2.mlstatic.com/D_NQ_NP_632155-MLB27524314306_062018-Z.jpg" srcset="https://http2.mlstatic.com/D_NQ_NP_632155-MLB27524314306_062018-Z.jpg" style="height: 70px" data-object-fit="cover">
-                                        </span>
-
-                                    <!-- Nome do produto -->
-                                    <div class="cart-page-heading">
-                                        <h5>Geforce Zotac Gtx Performance Nvidia Gtx 660 2gb Ddr5 192bit</h5>
-                                        <p>Quantidade: 1</p>
-                                    </div>
-
-                                    <!-- Preços do produto -->
-                                    <ul class="order-details-form">
-
-                                        <li><span>Produto</span><span>R$&nbsp;95,99</span></li>
-                                        <li><span>Envio</span><span>R$&nbsp;11,90</span></li>
-                                        <li><span>Total</span><span>R$&nbsp;107.89</span></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-
-                    <fieldset class="next col-md-12">
-                        <div class="row">
-                            <!-- Formulario de endereço -->
-                            <div class="col-md-7">
-
-                                <!-- Nome -->
-                                <div class="col-md-6">
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="number" class="form-control" name="cep" required>
-                                            <label class="form-label">Cep</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Estado e Cidade -->
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="form-group form-float col-md-6">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" name="estado" required>
-                                                <label class="form-label">Estado</label>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group form-float col-md-6">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" name="cidade" required>
-                                                <label class="form-label">Cidade</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Complemnto -->
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="form-group form-float col-md-6">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" name="endereco" required>
-                                                <label class="form-label">Rua/Avenida</label>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group form-float col-md-6">
-                                            <div class="form-line">
-                                                <input type="numero" class="form-control" name="numero" required>
-                                                <label class="form-label">Número</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Complemento -->
-                                <div class="col-md-12">
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="complemento" required>
-                                            <label class="form-label">Complemento</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Bairro -->
-                                <div class="col-md-6">
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="bairro" required>
-                                            <label class="form-label">Bairro</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <br><br><br>
-                                <br><br><br>
-                                <br><br><br>
-
-                                <!-- Botão continuar  -->
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-md-12" style="text-align: right">
-                                            <a class="btn" id="next" style="color: #fff; margin-top: 6px">Continuar &nbsp;<i class="fa fa-angle-double-right"></i> </a>
-                                            <input type="submit" class="btn">
-                                        </div>
-                                    </div>
-                                </div>
 
                             </div>
 
@@ -246,6 +118,10 @@
                         </div>
                     </fieldset>
                     <fieldset class="next">
+
+                    </fieldset>
+                    <fieldset class="next">
+
                     </fieldset>
                     <fieldset class="next">
 
