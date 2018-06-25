8675e34411686183cc921ef9482fcd822a6c6e74
Lelisvaldo Almeida Gomes <lelisvaldo@outlook.com>
2018-06-25 14:44:10 -0300

    Correção dos Forms de cores

--- resources/views/pages/admin/cadCor.blade.php
+++ resources/views/pages/admin/cadCor.blade.php
@@ -12,7 +12,6 @@
             outline:none;
         }
     </style>
-
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
@@ -24,109 +23,107 @@
             </ol>
         </section>
 
-
-        <section class="content">
-            @include('partials.admin._alerts')
-
-            <!-- Cadastro das cores -->
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                            <i class="fa fa-minus"></i>
-                        </button>
-                    </div>
-                </div>
-
-                <div class="box-body">
-                    <form action="{{ route('color.save') }}" method="post">
-                        {{ csrf_field() }}
-
-                        <div class="col-md-12">
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Cor</label>
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-paint-brush"></i></span>
-                                            <input id="nm_cor" type="text" minlength="0" maxlength="40" class="form-control" name="nm_cor">
+        <section class="content-header">
+            <div class="content">
+                @include('partials.admin._alerts')
+                <div class="row">
+                    <div class="">
+                        <!-- Cadastro das cores -->
+                        <div class="col-md-4 col-md-6">
+                            <div class="box box-primary">
+                                <div class="box-header with-border">
+                                    <div class="box-tools pull-right">
+                                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="box-body">
+                                    <form action="{{ route('color.save') }}" method="post">
+                                        {{ csrf_field() }}
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Cor</label>
+                                                <div class="input-group">
+                                                    <span class="input-group-addon"><i class="fa fa-paint-brush"></i></span>
+                                                    <input type="text" minlength="0" maxlength="40" class="form-control" name="nm_cor">
+                                                </div>
+                                            </div>
                                         </div>
+                                        <div class="col-md-12 text-right">
+                                            <button type="submit" id="btnSalvarCor" class="btn btn-success pull-right"><i class="fa fa-save"></i>&nbsp;&nbsp;Salvar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Alteração das cores -->
+                        <div class="col-md-7 col-md-8">
+                            <!-- Lista das cores cadastradas -->
+                            <div class="box box-info">
+                                <div class="box-header with-border">
+                                    <div class="box-tools pull-right">
+                                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
                                     </div>
                                 </div>
 
-                            <div>&nbsp;</div>
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-6 col-md-8" style="margin-left: 25% !important;">
+                                            <!-- Botão pesquisar -->
+                                            <div class="input-group">
+                                                <input type="search" class="form-control"  id="search" name="search"  placeholder="Buscar...">
+                                                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-md-4" >
+                                            <div class="dataTables_length" id="example1_length">
+                                                <!-- Botão LIMITS -->
+                                                <select name="example1_length" aria-controls="example1" class="form-control form-control-sm">
+                                                    <option selected value="10">10</option>
+                                                    <option value="25">25</option>
+                                                    <option value="50">50</option>
+                                                    <option value="100">100</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <!-- Tabelas dos produtos -->
+                                    <table class="table" id="table">
+                                        <thead>
+                                        <tr>
+                                            <th class="text-left">Código</th>
+                                            <th class="text-left">Cor</th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        @foreach($cores as $cor)
+                                            <tr class="text-center">
+                                                <td class="text-left">{{ $cor->cd_cor }}</td>
+                                                <td id="nome_cor" class="text-left">{{ $cor->nm_cor }}</td>
+                                                <td class="text-right">
+                                                    <button id="btn_editar" class="fa fa-pencil btn btn-outline-warning" style="color: #367fa9"></button>
+                                                    <button id="btn_salvar" class="fa fa-check btn btn-outline-success" style="color: #008d4c; display: none"></button>
+                                                    <button id="btn_cancelar" class="fa fa-remove btn btn-outline-danger" style="color: #cc0000; display: none"></button>
+                                                    <button id="btn_excluir" class="fa fa-trash btn btn-outline-warning" style="color: #cc0000"></button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="col-md-12 text-right">
-                            <button type="submit" id="btnSalvarCor" class="btn btn-success pull-right"><i class="fa fa-save"></i>&nbsp;&nbsp;Salvar</button>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-
-            <!-- Lista das cores cadastradas -->
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                            <i class="fa fa-minus"></i>
-                        </button>
                     </div>
-                </div>
-
-                <div class="box-body">
-
-                    <!-- Botão pesquisar -->
-                    <div style="padding-left: 70%">
-                        <div>
-                            <input type="search" id="search" value="" class="form-control">
-                        </div>
-                    </div>
-                    <div>&nbsp;</div>
-
-
-                    <div class="dataTables_length" style="padding-left: 90%" id="example1_length">
-                        <select name="example1_length" aria-controls="example1" class="form-control form-control-sm">
-                            <option value="10">10</option>
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                        </select>
-                    </div>
-
-                    <!-- Tabelas dos produtos -->
-
-
-                    <table class="table" id="table">
-                        <thead>
-                        <tr>
-                            <th class="text-center">Código</th>
-                            <th class="text-center">Cor</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        @foreach($cores as $cor)
-
-                            <tr class="text-center">
-                                <td>{{ $cor->cd_cor }}</td>
-                                <td id="nome_cor">{{ $cor->nm_cor }}</td>
-                                <td class="text-right">
-                                    <button id="btn_editar" class="fa fa-pencil btn btn-outline-warning" style="color: #367fa9"></button>
-                                    <button id="btn_salvar" class="fa fa-check btn btn-outline-success" style="color: #008d4c; display: none"></button>
-                                    <button id="btn_cancelar" class="fa fa-remove btn btn-outline-danger" style="color: #cc0000; display: none"></button>
-                                    <button id="btn_excluir" class="fa fa-trash btn btn-outline-warning" style="color: #cc0000"></button>
-                                </td>
-                            </tr>
-
-
-                        @endforeach
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </section>
     </div>
+
 
     <script src="{{asset('js/admin/jquery.blockUI.js')}}"></script>
     <script>
