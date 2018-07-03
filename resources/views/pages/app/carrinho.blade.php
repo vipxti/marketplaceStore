ee7e7c964c5d353eb593758bbc5c9c533fca1f9c
Unknown <fnasccunha@gmail.com>
2018-07-03 16:57:37 -0300

    Correção da pagina do carrinho

--- resources/views/pages/app/carrinho.blade.php
+++ resources/views/pages/app/carrinho.blade.php
@@ -53,14 +53,16 @@
                     </div>
 
                     <div class="cart-footer d-flex mt-30">
+
                         <div class="back-to-shop">
                             <button type="button" class="btn btn-danger" style="border-radius: 0px; font-size: 14px; background-color: #d33889">Continuar Comprando</button>
                         </div>
-                        <div class="update-checkout w-50">
+                        <div class="update-checkout w-50" style="padding: 0 4px">
                             <button type="button" class="btn btn-danger" style="border-radius: 0px; font-size: 14px; background-color: #3a3a3a">Limpar Carrinho</button>
                             <button type="button" class="btn btn-danger" style="border-radius: 0px; font-size: 14px; background-color: #3a3a3a">Atualizar Carrinho</button>
                         </div>
                     </div>
+
                 </div>
             </div>
 
@@ -69,17 +71,45 @@
                     <div class="coupon-code-area mt-70">
                         <div class="cart-page-heading">
                             <h5>Calcular Frete</h5>
-                            <p>Insira o CEP do endereço que deseja receber o produto.</p>
+                            <p><b>Insira o CEP do endereço que deseja receber o produto.</b></p>
                             <p>Assim você poderá calcular o frete e conhecer os serviços disponíveis</p>
                         </div>
                         <form>
-                            <input id="campoCep" type="text" name="cep_cliente" maxlength="8" style="padding: 0 10px !important; border: 0.5px solid #1e282c !important;">
-                            <button class="btn btn-danger" id="btnCalcFrete" type="button" style="border-radius: 0px !important; width: 90px !important;">Calcular</button><br>
+                            <input id="campoCep" type="text" name="cep_cliente" maxlength="8" style="padding: 0 10px !important;">
+                            <button class="btn btn-danger" id="btnCalcFrete" type="button" style="border-radius: 0px">Calcular</button>
+                            <i id="spinner_btn" class="fa fa-spinner fa-pulse fa-3x fa-fw" style="font-size: 22px; visibility: hidden"></i><br>
                             <p style="font-size: 0.90em" id="msgErroCep"></p>
                         </form>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 col-lg-6">
+                {{--<div class="col-12 col-md-6 col-lg-6">
+                    <div class="shipping-method-area mt-70">
+                        <div class="cart-page-heading">
+                            <h5>Qual envio prefere?</h5>
+                            <p>Escolha uma Opção</p>
+                        </div>
+                        <div class="custom-control custom-radio mb-14">
+                            <input type="radio" id="customRadio1" name="customRadio" value="2" class="custom-control-input" checked>
+                            <label class="custom-control-label d-flex align-items-center justify-content-between" for="customRadio1">
+                                <span id="sedex">Expresso &nbsp;</span><span id="precoSedex"></span><span id="diasSedex"></span>
+                            </label>
+                        </div>
+                        <div class="custom-control custom-radio mb-14">
+                            <input type="radio" id="customRadio2" name="customRadio" value="1" class="custom-control-input">
+                            <label class="custom-control-label d-flex align-items-center justify-content-between" for="customRadio2">
+                                <span id="pac">Normal &nbsp;</span><span id="precoPac"></span><span id="diasPac"></span>
+                            </label>
+                        </div>--}}{{--
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input" checked>
+                            <label class="custom-control-label d-flex align-items-center justify-content-between" for="customRadio3">
+                                <span>Retirar no Local</span>
+                                <span>Grátis</span>
+                            </label>
+                        </div>--}}{{--
+                    </div>
+                </div>--}}
+                <div class="col-12 col-md-6 col-lg-6" style="margin-top: 70px !important;">
                     <div class="col-md-12 col-lg-12">
                         <div class="cart-page-heading">
                             <h5>Qual envio prefere?</h5>
@@ -118,6 +148,66 @@
                                 <span id="diasSedex" ></span>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-12">
+                    <div class="cart-total-area mt-70">
+                        <div class="cart-page-heading">
+                            <h5>Total</h5>
+                            <p>Informações da compra</p>
+                        </div>
+                        <ul class="cart-total-chart">
+                            <li><span>Subtotal</span> <span id="precoSubTotal">-</span></li>
+                            <li><span>Envio</span> <span id="precoCalcFrete">-</span></li>
+                            <li><span><strong>Total</strong></span> <span><strong id="precoCalcTotal">-</strong></span></li>
+                        </ul>
+
+                        <form id="formComprar">
+                        {{ csrf_field() }}
+
+                        @foreach($produtos as $key => $produto)
+
+                            <!-- array dos produtos -->
+                                <input type="hidden" name="id[]" value="{{ $key + 1}}">
+                                <input type="hidden" name="code[]" id="code" value="{{ $produto['uidProduto'] }}" />
+                                <input type="hidden" name="descricao[]" value="{{ $produto['nomeProduto'] }}">
+                                <input type="hidden" id="qtdProd" name="quantidade[]" value="{{ $produto['qtdIndividual'] }}">
+                                <input type="hidden" name="valor[]" value="{{ $produto['valorProduto'] }}">
+                                <input type="hidden" name="peso[]" value="{{ $produto['pesoProduto'] }}">
+                                <input type="hidden" name="fretecal[]" value="">
+                                <input type="hidden" name="largura[]" value="{{ $produto['larguraProduto'] }}">
+                                <input type="hidden" name="altura[]" value="{{ $produto['alturaProduto'] }}">
+                                <input type="hidden" name="comprimento[]" value="{{ $produto['comprimentoProduto'] }}">
+                                <input id="freteForm" type="hidden" name="freteval[]" value="">
+                                <input id="tipoServForm" type="hidden" name="tipoServ[]" value="">
+
+                        @endforeach
+
+
+
+                        <!-- array do cliente -->
+                            {{--  <input type="hidden" name="cep" value="{{ $cliente[0]->cd_cep }}">
+                              <input type="hidden" name="endereco" value="{{ $cliente[0]->ds_endereco }}">
+                              <input type="hidden" name="numero" value="{{ $cliente[0]->cd_numero_endereco }}">
+                              <input type="hidden" name="bairro" value="{{ $cliente[0]->nm_bairro }}">
+                              <input type="hidden" name="cidade" value="{{ $cliente[0]->nm_cidade }}">
+                              <input type="hidden" name="estado" value="{{ $cliente[0]->nm_uf }}">
+                              <input type="hidden" name="pais" value="{{ $cliente[0]->nm_pais }}">--}}
+
+                            @if(Auth::check())
+
+                                <input type="hidden" name="email_cliente" value="{{ Auth::user()->email }}">
+                                <input type="hidden" name="nome" value="{{ Auth::user()->nm_cliente }}">
+                                <input type="hidden" name="numero_cpf" value="{{ Auth::user()->cd_cpf_cnpj }}">
+
+                            @endif
+
+
+                            {{--<input type="hidden" name="telefone" value="{{ $cliente[0]->fk_cd_telefone }}">
+                            <input type="hidden" name="data_nascimento" value="{{ $cliente[0]->dt_nascimento }}">--}}
+
+                            <button type="button" id="finalizar" disabled class="btn btn-danger" style="width: 100%; border-radius: 0px; font-weight: 700; font-size: 14px; background-color: #d33889">Finalizar Compra</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -398,6 +488,8 @@
                 }
 
             });
+
+
     </script>
 
 @stop
