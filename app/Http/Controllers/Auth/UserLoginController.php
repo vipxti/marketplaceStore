782f64508126e4c1f7159f477f5308f34d88059f
Unknown <fnasccunha@gmail.com>
2018-07-17 15:56:11 -0300

    Bug correction

--- app/Http/Controllers/Auth/UserLoginController.php
+++ app/Http/Controllers/Auth/UserLoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Category;
 use App\Http\Controllers\Controller;
+use App\Menu;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
@@ -19,28 +21,8 @@ class UserLoginController extends Controller
 
     public function showAdminLoginForm()
     {
-        $menuNav =  Menu::all();
 
-        //Carrega as categorias e subcategorias para serem apresentadas no menu nav
-        foreach($menuNav as $key=>$menu){
-
-            $categoriaSubCat[$key] = Category::
-            leftJoin('categoria_subcat', 'categoria.cd_categoria', '=', 'categoria_subcat.cd_categoria')
-                ->leftJoin('sub_categoria', 'sub_categoria.cd_sub_categoria', '=', 'categoria_subcat.cd_sub_categoria')
-                ->leftJoin('menu_categoria', 'menu_categoria.fk_cd_categoria', '=', 'categoria.cd_categoria')
-                ->leftJoin('menu', 'menu.cd_menu', '=', 'menu_categoria.fk_cd_menu')
-                ->select(
-                    'categoria.cd_categoria',
-                    'categoria.nm_categoria',
-                    'sub_categoria.cd_sub_categoria',
-                    'sub_categoria.nm_sub_categoria'
-                )
-                ->where('menu.cd_menu', '=', $menu->cd_menu)
-                ->get();
-
-        }
-
-        return view('pages.admin.auth.login', compact('menuNav', 'categoriaSubCat'));
+        return view('pages.admin.auth.login');
     }
 
     public function login(UserLoginRequest $request)
