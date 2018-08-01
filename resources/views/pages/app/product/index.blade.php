0a811ac832f496bd6ef4afd023f00342217ea3c8
Lelisvaldo Almeida Gomes <lelisvaldo@outlook.com>
2018-08-01 14:00:03 -0300

    Correção de estilo da home

--- resources/views/pages/app/product/index.blade.php
+++ resources/views/pages/app/product/index.blade.php
@@ -2,85 +2,85 @@
 
 @section('content')
 
-        <section class="shop_grid_area section_padding_100">
-            <div class="container">
-                <div class="row">
-                    <!-- Produtos e Contador páginas -->
-                    <div class="col-12 col-md-12 col-lg-12 justify-content-center">
-                        <div class="shop_grid_product_area">
-                            <div class="row">
-                                <div class="row karl-new-arrivals d-flex justify-content-center">
-                                    @foreach($produtos as $key => $produto)
-                                        <div class="col-12 col-sm-6 col-lg-4 single_gallery_item wow fadeInUpBig" data-wow-delay="0.2s">
-                                            <div class="product-img">
-                                                <img src="{{ URL::asset('img/products' . '/' . $produto->im_produto) }}" alt="{{ $produto->nm_slug }}">
-                                                <div class="product-quicview">
-                                                    <a href="{{ route('products.details', $produto->nm_slug) }}"><i class="ti-plus"></i></a>
-                                                </div>
+    <section class="shop_grid_area section_padding_100">
+        <div class="container">
+            <div class="row">
+                <!-- Produtos e Contador páginas -->
+                <div class="col-12 col-md-12 col-lg-12 justify-content-center">
+                    <div class="shop_grid_product_area">
+                        <div class="row">
+                            <div class="row karl-new-arrivals d-flex justify-content-center">
+                                @foreach($produtos as $key => $produto)
+                                    <div class="col-12 col-sm-6 col-lg-4 single_gallery_item wow fadeInUpBig" data-wow-delay="0.2s">
+                                        <div class="product-img">
+                                            <img src="{{ URL::asset('img/products' . '/' . $produto->im_produto) }}" alt="{{ $produto->nm_slug }}">
+                                            <div class="product-quicview">
+                                                <a href="{{ route('products.details', $produto->nm_slug) }}"><i class="ti-plus"></i></a>
                                             </div>
+                                        </div>
 
-                                            <div class="product-description text-center">
-                                                <h4 class="product-price">R$ {{ str_replace(".", ",", $produto->vl_produto) }}</h4>
-                                                <p>{{ $produto->nm_produto }}</p>
+                                        <div class="product-description text-center">
+                                            <h4 class="product-price">R$ {{ str_replace(".", ",", $produto->vl_produto) }}</h4>
+                                            <p>{{ $produto->nm_produto }}</p>
 
-                                                @if ($variation[$key]->cd_produto != null)
+                                            @if ($variation[$key]->cd_produto != null)
+
+                                                @if($produto->qt_produto < 5)
+                                                    <p style="font-weight: 600; color: #d59431; padding-top: 10px">SEM ESTOQUE</p>
+                                                @else
+                                                    <div class="col-12 col-md-12 d-flex justify-content-center">
+                                                        <a class="btn btn-link add-to-cart-btn" href="{{ route('products.details', $produto->nm_slug) }}" style="text-decoration: none; padding: 0"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; COMPRAR</a>
+                                                    </div>
+                                                @endif
+
+                                            @else
+
+                                            <!-- Botão comprar -->
+                                                <form action="{{ route('cart.buy') }}" method="post">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="cd_produto" value="{{ $produto->cd_produto }}">
+                                                    <input type="hidden" name="nm_produto" value="{{ $produto->nm_produto }}">
+                                                    <input type="hidden" name="ds_produto" value="{{ $produto->ds_produto }}">
+                                                    <input type="hidden" name="vl_produto" value="{{ $produto->vl_produto }}">
+                                                    <input type="hidden" name="qt_produto" value="{{ $produto->qt_produto }}">
+                                                    <input type="hidden" name="sku_produto" value="{{ $produto->cd_nr_sku }}">
+                                                    <input type="hidden" name="slug_produto" value="{{ $produto->nm_slug }}">
+                                                    <input type="hidden" name="ds_altura" value="{{ $produto->ds_altura }}">
+                                                    <input type="hidden" name="ds_largura" value="{{ $produto->ds_largura }}">
+                                                    <input type="hidden" name="ds_comprimento" value="{{ $produto->ds_comprimento }}">
+                                                    <input type="hidden" name="ds_peso" value="{{ $produto->ds_peso }}">
+                                                    <input type="hidden" name="im_produto" value="{{ $produto->im_produto }}">
 
                                                     @if($produto->qt_produto < 5)
                                                         <p style="font-weight: 600; color: #d59431; padding-top: 10px">SEM ESTOQUE</p>
                                                     @else
                                                         <div class="col-12 col-md-12 d-flex justify-content-center">
-                                                            <a class="btn btn-link add-to-cart-btn" href="{{ route('products.details', $produto->nm_slug) }}" style="text-decoration: none; padding: 0"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; COMPRAR</a>
+                                                            <button type="submit" class="btn btn-link add-to-cart-btn" style="text-decoration: none; padding: 0"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; COMPRAR</button>
                                                         </div>
                                                     @endif
-                                                    
-                                                @else
+                                                </form>
 
-                                                    <!-- Botão comprar -->
-                                                    <form action="{{ route('cart.buy') }}" method="post">
-                                                        {{ csrf_field() }}
-                                                        <input type="hidden" name="cd_produto" value="{{ $produto->cd_produto }}">
-                                                        <input type="hidden" name="nm_produto" value="{{ $produto->nm_produto }}">
-                                                        <input type="hidden" name="ds_produto" value="{{ $produto->ds_produto }}">
-                                                        <input type="hidden" name="vl_produto" value="{{ $produto->vl_produto }}">
-                                                        <input type="hidden" name="qt_produto" value="{{ $produto->qt_produto }}">
-                                                        <input type="hidden" name="sku_produto" value="{{ $produto->cd_nr_sku }}">
-                                                        <input type="hidden" name="slug_produto" value="{{ $produto->nm_slug }}">
-                                                        <input type="hidden" name="ds_altura" value="{{ $produto->ds_altura }}">
-                                                        <input type="hidden" name="ds_largura" value="{{ $produto->ds_largura }}">
-                                                        <input type="hidden" name="ds_comprimento" value="{{ $produto->ds_comprimento }}">
-                                                        <input type="hidden" name="ds_peso" value="{{ $produto->ds_peso }}">
-                                                        <input type="hidden" name="im_produto" value="{{ $produto->im_produto }}">
+                                            @endif
 
-                                                        @if($produto->qt_produto < 5)
-                                                            <p style="font-weight: 600; color: #d59431; padding-top: 10px">SEM ESTOQUE</p>
-                                                        @else
-                                                            <div class="col-12 col-md-12 d-flex justify-content-center">
-                                                                <button type="submit" class="btn btn-link add-to-cart-btn" style="text-decoration: none; padding: 0"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; COMPRAR</button>
-                                                            </div>
-                                                        @endif
-                                                    </form>
-                                                    
-                                                @endif
-                                                
-                                            </div>
                                         </div>
-                                    @endforeach
-                                </div>
-                                <p>&nbsp;</p><p>&nbsp;</p>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 col-md-12 d-flex justify-content-center">
-                                    <div class="shop_pagination_area wow fadeInUp" data-wow-delay="1.1s">
-                                        <nav aria-label="Page navigation">
-                                            {{ $produtos->links() }}
-                                        </nav>
                                     </div>
+                                @endforeach
+                            </div>
+                            <p>&nbsp;</p><p>&nbsp;</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-md-12 d-flex justify-content-center">
+                                <div class="shop_pagination_area wow fadeInUp" data-wow-delay="1.1s">
+                                    <nav aria-label="Page navigation">
+                                        {{ $produtos->links() }}
+                                    </nav>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
 @stop
--- resources/views/partials/app/_footer.blade.php
+++ resources/views/partials/app/_footer.blade.php
@@ -1,174 +1,112 @@
-    {{--<footer class="footer_area">
+<!-- Footer-->
+<footer class="main-footer">
+    <!-- Service Block-->
+    <div class="services-block">
         <div class="container">
             <div class="row">
-                <!-- Single Footer Area Start -->
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="single_footer_area">
-                        <div class="footer-logo">
-                            <a class="text-center" href="{{ route('index')}}"><img style="max-width: 35%" src=" {{ asset('img/app/core-img/logo.png') }}" alt=""></a>
-                        </div>
-                        <div class="copywrite_text d-flex align-items-center">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                <script>document.write(new Date().getFullYear());</script> &copy; Todos os direitos reservados <i style="color: #d33889" class="fa fa-heart-o" aria-hidden="true"></i> by <a href="http://vipx.com.br/" target="_blank" style="color: #d33889">VipX Marketplace</a>
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                <!-- frete Grátis -->
+                <div class="col-lg-4 d-flex justify-content-center justify-content-lg-start">
+                    <div class="item d-flex align-items-center">
+                        <div class="icon"><i class="icon-truck"></i></div>
+                        <div class="text">
+                            <h6 class="no-margin text-uppercase">Entregamos em todo Pais</h6><span>Entrega via Correio</span>
                         </div>
                     </div>
                 </div>
-                <!-- Single Footer Area Start -->
-                <div class="col-12 col-sm-6 col-md-3 col-lg-2">
-                    <div class="single_footer_area">
-                        <ul class="footer_widget_menu">
-                            <li><a href="#">Minha Conta</a></li>
-                            <li><a href="#">Sobre</a></li>
-                            <li><a href="#">Perguntas frequentes</a></li>
-                            <li><a href="#">Nossas Politicas</a></li>
-                            <li><a href="#">Atendimento</a></li>
-                            <li><a href="#">Contato</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Single Footer Area Start -->
-                <div>
-                    <div class="single_footer_area">
-                        <ul class="footer_widget_menu">
-                            <div class="footer_social_area">
-                                <a href="https://www.facebook.com/shoppingvipx/" target="_blank" style="font-size: 20px"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="https://www.instagram.com/shoppingvipx/" target="_blank" style="font-size: 20px"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                <a href="https://twitter.com/vipxmarketplace" target="_blank" style="font-size: 20px"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="https://www.youtube.com/user/VipXModaFeminina" target="_blank" style="font-size: 20px"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Single Footer Area Start -->
-                <div class="col-12 col-lg-5">
-                    <div class="single_footer_area">
-                        <div class="footer_heading mb-30">
-                            <h6>Assine a nossa newsletter</h6>
-                        </div>
-                        <div class="subscribtion_form">
-                            <form action="#" method="post">
-                                <input type="email" name="mail" class="mail" placeholder="Digite seu email aqui">
-                                <button type="submit" class="submit">Se inscrever</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>--}}
 
-    <!-- Footer-->
-    <footer class="main-footer">
-        <!-- Service Block-->
-        <div class="services-block">
-            <div class="container">
-                <div class="row">
-                    <!-- frete Grátis -->
-                    <div class="col-lg-4 d-flex justify-content-center justify-content-lg-start">
-                        <div class="item d-flex align-items-center">
-                            <div class="icon"><i class="icon-truck"></i></div>
-                            <div class="text">
-                                <h6 class="no-margin text-uppercase">Entregamos em todo Pais</h6><span>Entrega via Correio</span>
-                            </div>
+                <!-- Pagamento Seguro -->
+                <div class="col-lg-4 d-flex justify-content-center">
+                    <div class="item d-flex align-items-center">
+                        <div class="icon"><i class="icon-secure-shield"></i></div>
+                        <div class="text">
+                            <h6 class="no-margin text-uppercase">Pagamento Seguro</h6><span>PagSeguro</span>
                         </div>
                     </div>
+                </div>
 
-                    <!-- Pagamento Seguro -->
-                    <div class="col-lg-4 d-flex justify-content-center">
-                        <div class="item d-flex align-items-center">
-                            <div class="icon"><i class="icon-secure-shield"></i></div>
-                            <div class="text">
-                                <h6 class="no-margin text-uppercase">Pagamento Seguro</h6><span>PagSeguro</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Suporte -->
-                    <div class="col-lg-4 d-flex justify-content-center">
-                        <div class="item d-flex align-items-center">
-                            <div class="icon"><i class="icon-headphones"></i></div>
-                            <div class="text">
-                                <h6 class="no-margin text-uppercase">13 97424-8882</h6><span>Suporte de Segunda à Sábado</span>
-                            </div>
+                <!-- Suporte -->
+                <div class="col-lg-4 d-flex justify-content-center">
+                    <div class="item d-flex align-items-center">
+                        <div class="icon"><i class="icon-headphones"></i></div>
+                        <div class="text">
+                            <h6 class="no-margin text-uppercase">13 97424-8882</h6><span>Suporte de Segunda à Sábado</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Main Block -->
-        <div class="main-block">
-            <div class="container">
-                <div class="row">
-                    <div class="info col-lg-4">
-                        <div class="logo"><img src="{{asset('img/logo.png')}}" href="{{ route('index')}}"></div>
-                        <p></p>
-                        <ul class="social-menu list-inline">
-                            <li class="list-inline-item"><a href="https://twitter.com/vipxmarketplace" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
-                            <li class="list-inline-item"><a href="https://www.facebook.com/MaktubBeauty/?ref=br_rs" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
-                            <li class="list-inline-item"><a href="https://www.instagram.com/maktubbeauty/" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
-                            <li class="list-inline-item"><a href="https://www.youtube.com/user/VipXModaFeminina" target="_blank" title="pinterest"><i class="fa fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="site-links col-lg-2 col-md-6">
-                        <h5 class="text-uppercase">Dúvidas</h5>
-                        <ul class="list-unstyled">
-                            <li> <a href="#">Frete Grátis</a></li>
-                            <li> <a href="#">Trocas e Devoluções</a></li>
-                            <li> <a href="#">Prazos e Envios</a></li>
-                            <li><a href="#">Loja Segura</a></li>
-                        </ul>
-                    </div>
-                    <div class="site-links col-lg-2 col-md-6">
-                        <h5 class="text-uppercase">Empresa</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Central de Atendimento</a></li>
-                            <li><a href="#">Quem Somos</a></li>
-                            @if(Auth::check())
-                                <li>
-                                    <a href="{{ route('client.dashboard') }}">Minha Conta</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('client.logout') }}">Sair</a>
-                                </li>
-                            @else
-                                <li>
-                                    <a href="{{ route('client.login') }}">Fazer login</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('client.register') }}">Cadastrar-se</a>
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
-                    <div class="newsletter col-lg-4">
-                        <h5 class="text-uppercase">Assine a nossa newsletter</h5>
-                        <p style="color: #d59431">Deixe seu e-mail e receba os melhores preços e ofertas.</p>
-                        <form action="#" id="newsletter-form">
-                            <div class="form-group">
-                                <input type="email" name="subscribermail" placeholder="Digite seu email aqui">
-                                <button type="submit"> <i class="fa fa-paper-plane"></i></button>
-                            </div>
-                        </form>
-                    </div>
+    </div>
+    <!-- Main Block -->
+    <div class="main-block">
+        <div class="container">
+            <div class="row">
+                <div class="info col-lg-4">
+                    <div class="logo"><img src="{{asset('img/app/core-img/logo.png')}}" href="{{ route('index')}}"></div>
+                    <p></p>
+                    <ul class="social-menu list-inline">
+                        <li class="list-inline-item"><a href="https://www.facebook.com/MaktubBeauty/?ref=br_rs" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item" ><a href="https://www.instagram.com/maktubbeauty/" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+                <div class="site-links col-lg-2 col-md-6">
+                    <h5 class="text-uppercase">Dúvidas</h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="#">Frete Grátis</a></li>
+                        <li> <a href="#">Trocas e Devoluções</a></li>
+                        <li> <a href="#">Prazos e Envios</a></li>
+                        <li><a href="#">Loja Segura</a></li>
+                    </ul>
+                </div>
+                <div class="site-links col-lg-2 col-md-6">
+                    <h5 class="text-uppercase">Empresa</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Central de Atendimento</a></li>
+                        <li><a href="#">Quem Somos</a></li>
+                        @if(Auth::check())
+                            <li>
+                                <a href="{{ route('client.dashboard') }}">Minha Conta</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('client.logout') }}">Sair</a>
+                            </li>
+                        @else
+                            <li>
+                                <a href="{{ route('client.login') }}">Fazer login</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('client.register') }}">Cadastrar-se</a>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+                <div class="newsletter col-lg-4">
+                    <h5 class="text-uppercase">Assine a nossa newsletter</h5>
+                    <p style="color: #d59431">Deixe seu e-mail e receba os melhores preços e ofertas.</p>
+                    <form action="#" id="newsletter-form">
+                        <div class="form-group">
+                            <input type="email" name="subscribermail" placeholder="Digite seu email aqui">
+                            <button type="submit"> <i class="fa fa-paper-plane"></i></button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <div class="copyrights">
-            <div class="container">
-                <div class="row d-flex align-items-center">
-                    <div class="text col-md-6">
-                        <p>&copy; 2018  <a href="https://vipx.com.br" target="_blank" style="color: #d59431; text-decoration: none">Vip-X Marketplace </a> Todos os direitos reservados.</p>
-                    </div>
-                    <div class="payment col-md-6 clearfix">
-                        <ul class="payment-list list-inline-item pull-right">
-                            <li class="list-inline-item"><img src="{{asset('img/visa.svg')}}"></li>
-                            <li class="list-inline-item"><img src="{{asset('img/mastercard.svg')}}"></li>
-                            <li class="list-inline-item"><img src="{{asset('img/paypal.svg')}}"></li>
-                            <li class="list-inline-item"><img src="{{asset('img/western-union.svg')}}"></li>
-                        </ul>
-                    </div>
+    </div>
+    <div class="copyrights">
+        <div class="container">
+            <div class="row d-flex align-items-center">
+                <div class="text col-md-6">
+                    <p>&copy; 2018  <a href="https://vipx.com.br" target="_blank" style="color: #d59431; text-decoration: none">Vip-X Marketplace </a> Todos os direitos reservados.</p>
+                </div>
+                <div class="payment col-md-6 clearfix">
+                    <ul class="payment-list list-inline-item pull-right">
+                        <li class="list-inline-item"><img src="{{asset('img/app/bg-img/visa.svg')}}"></li>
+                        <li class="list-inline-item"><img src="{{asset('img/app/bg-img/mastercard.svg')}}"></li>
+                        <li class="list-inline-item"><img src="{{asset('img/app/bg-img/paypal.svg')}}"></li>
+                        <li class="list-inline-item"><img src="{{asset('img/app/bg-img/western-union.svg')}}"></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
