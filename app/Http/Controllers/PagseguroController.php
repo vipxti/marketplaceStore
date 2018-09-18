f8bb812eb85839da42aaa8e9b9ae346b6d544758
felipecunha4 <fnasccunha@gmail.com>
2018-09-18 11:59:26 -0300

    pagseguro atualização de status manual

--- app/Http/Controllers/PagseguroController.php
+++ app/Http/Controllers/PagseguroController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class PagseguroController extends Controller
@@ -11,23 +12,41 @@ class PagseguroController extends Controller
         return view('pages.admin.pagSeguroIndex');
     }
 
-    public function consultaPedido(){
-
-            $token ='4D97A178277542CAAB150D1096002DF1';
+    public function consultaPedido(Request $request){
+        //dd($request->datas);
+        $token ='4D97A178277542CAAB150D1096002DF1';
         $emailPagseguro = 'vendas@vipx.com.br';
+        $dataInicial = date('Y-m-d', strtotime($request->datas[0]));
+        $dataFinal= date('Y-m-d', strtotime($request->datas[1]));
 
         //$cod_transacao = '3A448AD1-3381-4153-B28C-64D305074395';
-        $cod_transacao = '4125075B-DD02-410B-B809-2A3D38C65E14';
 
-        //$url = 'https://ws.pagseguro.uol.com.br/v3/transactions/' . $cod_transacao . '?email=' . $emailPagseguro;
-        $url = 'https://ws.pagseguro.uol.com.br/v2/transactions?'. $emailPagseguro .
-                '&initialDate=2018-10-01T00:00
-                &finalDate=2018-09-17T00:00
-                &page=1
-                &maxPageResults=100';
+        $url = 'https://ws.pagseguro.uol.com.br/v2/transactions?email='. $emailPagseguro .
+                '&initialDate=' . $dataInicial . 'T00:00'.
+                '&finalDate=' . $dataFinal . 'T00:00'.
+                '&page='. $request->pagina.
+                '&maxPageResults=100';
+
+        //dd($url);
 
         $xml = $this->executeGetTransaction($url, $token);
         $dados = simplexml_load_string($xml);
+        //dd($xml);
+
+        return response()->json([$dados]);
+    }
+
+    public function consultaPedidoTransacao(Request $request){
+        $token ='4D97A178277542CAAB150D1096002DF1';
+        $emailPagseguro = 'vendas@vipx.com.br';
+
+        $cod_transacao = $request->transacao;
+
+        $url = 'https://ws.pagseguro.uol.com.br/v3/transactions/' . $cod_transacao . '?email=' . $emailPagseguro;
+
+        $xml = $this->executeGetTransaction($url, $token);
+        $dados = simplexml_load_string($xml);
+        //dd($xml);
 
         return response()->json([$dados]);
     }
@@ -42,6 +61,27 @@ class PagseguroController extends Controller
         $response = curl_exec($curl_handle);
         curl_close($curl_handle);
         return $response;
+    }
+
+    public function atualizaPedidos(Request $request){
+        try {
+            $pedido = Order::where('cd_referencia', '=', $request->referencia)->get();
+
+            if (count($pedido) > 0) {
+                if ($pedido[0]->cd_status != $request->status) {
+                    Order::where('cd_referencia', '=', $request->referencia)
+                        ->update(['cd_status' => $request->status]);
+
+                    return response()->json(['deuErro' => false, 'statusAtualizado' => true]);
+                }
+            }
+        }
+        catch (\Exception $ex){
+            return response()->json(['deuErro' => true]);
+        }
+
+        return response()->json(['deuErro' => false, 'statusAtualizado' => false]);
+
     }
 
     public function urlRetorno(Request $request){
